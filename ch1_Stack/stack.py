1b1d67e445eb357e7e9040285d73a03a5c3bee64
appleboy919 <doh24@wisc.edu>
2021-02-28 17:41:17 +0900

    add: stack implementation

--- ch1_Stack/stack.py
+++ ch1_Stack/stack.py
@@ -1,5 +1,39 @@
+# implementing Stack data structure
+
+class Stack:
+    def __init__(self):
+        self.itmes = []
+
+    def is_empty(self):
+        # return len(self.items) == 0
+        return not self.items
+
+    def push(self, newItem):
+        self.itmes.append(newItem)
+
+    def pop(self):
+        return self.itmes.pop()
+
+    def peek(self):
+        return self.itmes[-1]
+
+    def size(self):
+        return len(self.itmes)
+
+    def __str__(self):
+        return str(self.itmes)
+
+if __name__ == '__main__':
+    stack = Stack()
+    print(stack)
+    for i in range(3):
+        stack.push(i)
+    print(stack)
+
 ### Note ###
 # DS in which all insertion and deletions are made at one end, called "top of the stack"
 # LIFO
 # Common operation: push, pop, peek, is_empty
-# Application: reverse Polish notation for evaluating arithmetic expressions, syntax parsing, cold stack(space for para and local vars is created), used in recursion, undo, redo operations, low-level assembly programming
+# Application: reverse Polish notation for evaluating arithmetic expressions,
+#               syntax parsing, cold stack(space in CPU for param and local vars is created),
+#               used in recursion, undo, redo operations, low-level assembly programming
