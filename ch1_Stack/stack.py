f41ca3b18fd9aa15c7b48e677c584bdeffd4b5fa
appleboy919 <doh24@wisc.edu>
2021-02-28 17:21:13 +0900

    "Stack data structure and its implementation"

---
+++ ch1_Stack/stack.py
@@ -0,0 +1,5 @@
+### Note ###
+# DS in which all insertion and deletions are made at one end, called "top of the stack"
+# LIFO
+# Common operation: push, pop, peek, is_empty
+# Application: reverse Polish notation for evaluating arithmetic expressions, syntax parsing, cold stack(space for para and local vars is created), used in recursion, undo, redo operations, low-level assembly programming
