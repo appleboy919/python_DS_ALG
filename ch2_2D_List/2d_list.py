10b980e579a53e34f8084025e34bc7d3ed2ef75f
appleboy919 <doh24@wisc.edu>
2021-03-04 23:01:59 +0900

    new: 2D list

---
+++ ch2_2D_List/2d_list.py
@@ -0,0 +1,9 @@
+# 2D List application:
+    # grids (screen pixels)
+    # tabular data (spreadsheet, table)
+    # matrices ==> "Graphs"
+    # game boards
+    # storing, accessing data from scientific experiments
+    # 2D indices can be used to access DOM elements
+
+# 2D list = list of lists
